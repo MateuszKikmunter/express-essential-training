c2151c8d2c40d18c02c29a792dda06f95e11352a
m.kikmunter <M.Kikmunter@gmail.com>
2020-03-12 22:18:42 +0000

    static data intro

--- express-app/index.js
+++ express-app/index.js
@@ -1,8 +1,20 @@
 import express from "express";
+import data from './../data/MOCK_DATA.json';
 
 const app = express();
 const PORT = 3000;
 
+//defining static data
+app.use(express.static("public"));
+
+//defining static for specific path and folder
+app.use("/images", express.static("images"));
+
+app.get("/", (req, res) => {
+    res.json(data);
+});
+
 app.listen(PORT, () => {
     console.log(`Your server is running on port ${PORT}`);
 });
+
